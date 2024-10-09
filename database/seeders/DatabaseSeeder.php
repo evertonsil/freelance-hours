ba909d6942724b5e54ef588fdd7fcc98fb0dd75d
evertonsil <everton.marques@uni9.edu.br>
2024-10-09 17:29:27 -0300

    criado seeder para inserir dados na tabela proposal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Proposal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,10 @@ class DatabaseSeeder extends Seeder
         //fazendo uma consulta com 10 usuários de forma randômica
         User::query()->inRandomOrder()->limit(10)->get()
             //a cada usuário cria um novo projeto
-            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]));
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
