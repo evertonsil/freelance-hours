f661747ca9f63eb577ebf6ff38a834a08d8027c5
evertonsil <everton.marques@uni9.edu.br>
2024-10-15 16:23:21 -0300

    criado validação dos dados do formulário de nova proposta

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Proposals;
 
 use App\Models\Project;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Create extends Component
@@ -12,15 +13,33 @@ class Create extends Component
 
     public bool $modal = false;
 
+    //adicionando regra de validação de email
+    #[Rule(['required', 'email'])]
     public string $email = '';
+    //adicionando regra de validação do campo de horas
+    #[Rule(['required', 'numeric', 'min:1'])]
     public int $hours = 0;
+
+    public bool $agree = false;
 
     public function save()
     {
+
+        //validação dos dados antes de salvar uma proposta
+        $this->validate();
+
+        //validação do aceite de termos
+        if (!$this->agree) {
+            $this->addError('agree', 'Você precisa concordar com os termos');
+            return;
+        }
+
         $this->project->proposals()->updateOrCreate(
             ['email' => $this->email],
             ['hours' => $this->hours]
         );
+
+        $this->modal = false;
     }
 
     public function render()
