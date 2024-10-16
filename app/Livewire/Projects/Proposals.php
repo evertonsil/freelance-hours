031064d4bf74b1ef4a6989deedf73ab3fcd4e333
evertonsil <everton.marques@uni9.edu.br>
2024-10-16 10:21:42 -0300

    ajustado exibição da listagem de propostas

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -4,11 +4,31 @@ namespace App\Livewire\Projects;
 
 use Livewire\Component;
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 
 class Proposals extends Component
 {
     //adiciona o projeto como uma propriedade pública de proposals(propostas)
     public Project $project;
+
+    public int $qty = 10;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()->orderBy('hours')->paginate($this->qty);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->qty += 10;
+    }
 
     public function render()
     {
